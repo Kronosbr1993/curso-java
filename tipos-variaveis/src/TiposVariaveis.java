5ac3ce8bdf0f8149a073bd26113e45e154d5a8ac
Kronos <thiagoriegeldemoura@gmail.com>
2025-02-24 22:57:26 -0300

    alteraçoes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2025;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("CEP: " + cep);
+        System.out.println("CPF: " + cpf);
+        System.out.println("PI: " + pi);
+        System.out.println("Salario: " + salario);
+
     }
 }
